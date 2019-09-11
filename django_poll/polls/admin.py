a0d9f9a5d176ab0b59d3347554e64e5587bc122f
Alexander Tallqvist <alexander_tallqvist@hotmail.com>
2019-09-11 21:01:57 +0300

    Started implementing our first routes.

--- django_poll/polls/admin.py
+++ django_poll/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+admin.site.site_header = "Django Poll Admin"
+admin.site.site_title = "Django Poll Admin"
+admin.site.index_title = "Welcome to Django Poll Admin"
+
+
+class ChoiseInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiseInline]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
